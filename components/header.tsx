72e5fd157505f67e9a2871adc7a224ec423a8dbd
warrendewey <warrendewey8@gmail.com>
2024-08-02 01:32:28 -0500

    Created header

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <main>
+      <Link href="/">Dewey Jewelry Co.</Link>
+      <div>Upload</div>;
+    </main>
+  );
+}
