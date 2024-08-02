a17b9bd88b7880dc1d1e27af3d5684f5ee80877d
warrendewey <warrendewey8@gmail.com>
2024-08-02 01:38:20 -0500

    More header work

--- components/header.tsx
+++ components/header.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <main>
-      <Link href="/">Dewey Jewelry Co.</Link>
-      <div>Upload</div>;
-    </main>
+    <nav className="w-full border-0 py-4 lg:px-24 px -10 bg-gray-950">
+      <h1 className="text-3xl text-white">
+        <Link href="/">Dewey Jewelry Co.</Link>
+        <div>Upload</div>;
+      </h1>
+    </nav>
   );
 }
