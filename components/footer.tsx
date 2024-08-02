df264de82b08285664652c2751ed2ccd372ec54b
warrendewey <warrendewey8@gmail.com>
2024-08-02 14:25:46 -0500

    Change layout details

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Footer() {
-  return <div></div>;
+export default function Footer({ font }: { font?: string }) {
+  return (
+    <footer className="py-12 bg-gray-952 text-gray-953">
+      <div
+        className={"max-w-[100rem] px-12 mx-auto flex justify-between ${font}"}
+      >
+        <p className="text-xl">Dewey Jewelry Co.</p>
+      </div>
+    </footer>
+  );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-export default function Header() {
+export default function Header({ font }: { font?: string }) {
   return (
     <header className="py-2 bg-gray-952">
       <div className="max-w-[100rem] px-12 mx-auto flex justify-between">
